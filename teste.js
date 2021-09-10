fc9ded5757524e6c21054047779f233ee4e2ddd5
Felipe Grassi Ribon <felipe.gribon@hotmail.com>
2021-09-10 01:45:33 -0300

    subindo
    finalizado

---
+++ teste.js
@@ -0,0 +1,7 @@
+var xx = [
+    {description: 'carro', amount: -120000, date: '01/08/2021'},
+    {description: 'aluguel', amount: -80000, date: '15/08/2021'},
+    {description: 'aluguel', amount: -80000, date: '15/09/2021'},
+    {description: 'salario', amount: 500000, date: '30/08/2021'}
+]
+console.log(xx[3].description)
